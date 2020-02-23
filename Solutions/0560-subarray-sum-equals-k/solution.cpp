002401544b18a2aa2675785a2d3fd27994a98343
Raihan Khan <raihan@appscode.com>
2020-02-23 11:05:57 +0000

    [LeetCode Sync] Runtime - 24 ms (100.00%), Memory - 14.2 MB (100.00%)

---
+++ Solutions/0560-subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& A, int B)
+    {
+    
+    unordered_map<int ,int> mm ;               
+    
+    int sz=A.size(),i,currsum=0,tot=0;
+    
+    for(i=0;i<sz;i++)
+    {
+        currsum+=A[i];                      
+        
+        if(mm.find(currsum-B)!=mm.end())
+        tot+=mm[currsum-B];
+        
+        mm[currsum]++;
+        if(mm.find(currsum)->first==B)
+            tot++;
+    }
+    return tot;
+    }
+};
