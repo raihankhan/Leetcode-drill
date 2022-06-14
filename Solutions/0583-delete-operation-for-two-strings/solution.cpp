f6fa2c33d3360dbd22074763666ead780c1a68c9
Raihan Khan <raihan@appscode.com>
2022-06-14 15:36:53 +0000

    [LeetCode Sync] Runtime - 4 ms (96.17%), Memory - 7.6 MB (100.00%)

--- Solutions/0583-delete-operation-for-two-strings/solution.cpp
+++ Solutions/0583-delete-operation-for-two-strings/solution.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int len1=word1.length();
-        int len2=word2.length();
+        int n=word1.size(),m=word2.size();
+        int dp[n+1][m+1];
+        memset(dp , 0 , sizeof(dp));
         
-        int dp[len1+1][len2+1];
-        
-        dp[0][0]=0;
-        
-        for(int i=1;i<=len1;i++)
-            dp[i][0]=i;
-        
-        for(int j=1;j<=len2;j++)
-            dp[0][j]=j;
-        
-        for(int i=1;i<=len1;i++)
-        {
-            for(int j=1;j<=len2;j++)
-            {
-                if(word1[i-1]==word2[j-1])
-                    dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=1+min(dp[i-1][j],dp[i][j-1]);
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(word1[i-1]==word2[j-1]) {
+                    dp[i][j]=1+dp[i-1][j-1];
+                } else {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
         
-        return dp[len1][len2];
+        return n+m-(dp[n][m]<<1);
     }
 };
