6776ecd996849ab2bf042f36b7d7e62e225901eb
Raihan Khan <raihan@appscode.com>
2022-10-07 06:48:43 +0000

    [LeetCode Sync] Runtime - 275 ms (5.20%), Memory - 52.3 MB (100.00%)

---
+++ Solutions/0217-contains-duplicate/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,bool>appearedOnce;
+        for(int i=0;i<nums.size();i++) {
+            if(appearedOnce[nums[i]]) return true;
+            appearedOnce[nums[i]]=true;
+        }
+        return false;
+    }
+};
