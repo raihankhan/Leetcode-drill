71f656fb2f26d0de590311cd7c58d10e3808e8ef
Raihan Khan <raihan@appscode.com>
2022-10-19 11:08:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.8 MB (100.00%)

--- Solutions/0070-climbing-stairs/solution.cpp
+++ Solutions/0070-climbing-stairs/solution.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[50]={0};
-        dp[1]=1;
-        dp[2]=2;
-        
-        for(int i=3;i<=n;i++)
-        {
+        int dp[n];
+        dp[0]=1;
+        if(n>=2) dp[1]=2;
+        for(int i=2;i<n;i++) {
             dp[i]=dp[i-1]+dp[i-2];
         }
-        
-        return dp[n];
-        
+        return dp[n-1];
     }
 };
