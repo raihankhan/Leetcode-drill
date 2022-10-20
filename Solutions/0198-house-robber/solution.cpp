cf4686fd9fe13129b0404a8b2a34c2afe3487773
Raihan Khan <raihan@appscode.com>
2022-10-20 08:24:39 +0000

    [LeetCode Sync] Runtime - 3 ms (33.67%), Memory - 7.6 MB (100.00%)

--- Solutions/0198-house-robber/solution.cpp
+++ Solutions/0198-house-robber/solution.cpp
@@ -1,17 +1,31 @@
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         int dp[n];
+//         dp[0]=nums[0];
+//         if(n>1) dp[1]=max(nums[0],nums[1]);
+//         for(int i=2;i<n;i++) {
+//             dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+//         }
+//         return dp[n-1];
+//     }
+// };
+
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int len=nums.size();
-        if(len==0) return 0;
-        int dp[len+2];
-        memset(dp,0,sizeof(dp));
-        dp[0]=nums[0];
-        if(len>1) dp[1]=max(dp[0],nums[1]);
-        for(int i=2;i<nums.size();i++)
-        {
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        int n=nums.size();
+        if(n==1) return nums[0];
+        int prev1,prev2;
+        prev2=nums[0];
+        prev1=max(prev2,nums[1]);
+        for(int i=2;i<n;i++) {
+            int currPrev1=prev1;
+            prev1=max(currPrev1,prev2+nums[i]);
+            prev2=currPrev1;
         }
-        
-        return dp[len-1];
+        return prev1;
     }
 };
