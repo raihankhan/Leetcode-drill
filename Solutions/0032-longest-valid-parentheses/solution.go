3ec210ba45d971b59e964d3ff737dbfcc35ce733
Raihan Khan <raihan@appscode.com>
2024-06-26 11:44:56 +0000

    [LeetCode Sync] Runtime - 11 ms (11.50%), Memory - 3.7 MB (28.76%)

---
+++ Solutions/0032-longest-valid-parentheses/solution.go
@@ -0,0 +1,20 @@
+func longestValidParentheses(s string) int {
+    stack := []int{-1}
+    maxLen := 0
+
+    for i, c := range s {
+        if c == '(' {
+            stack = append(stack, i)
+        } else { // c == ')'
+            stack = stack[:len(stack)-1]
+            if len(stack) > 0 {
+                fmt.Println(i)
+                maxLen = max(maxLen, i-stack[len(stack)-1])
+            } else {
+                stack = append(stack, i)
+            }
+        }
+    }
+
+    return maxLen
+}
